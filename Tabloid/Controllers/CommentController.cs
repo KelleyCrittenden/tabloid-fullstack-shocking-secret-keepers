f29f25903a8638e974d77aefe4bb03254ac2ffb9
Sisi Freeley <liusi33@gmail.com>
2020-10-08 15:23:58 -0500

    deleted commented out code

--- Tabloid/Controllers/CommentController.cs
+++ Tabloid/Controllers/CommentController.cs
@@ -64,64 +64,6 @@ namespace Tabloid.Controllers
 
         }
 
-
-
-
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Update(int id, Comment comment)
-        //{
-
-        //    try
-        //    {
-
-        //        //comment.UserProfileId = userId;
-        //        _commentRepository.UpdateComment(comment);
-        //        return RedirectToAction("Details", new { id = comment.Id });
-        //    }
-        //    catch
-        //    {
-        //        //    return View(comment);
-        //        //}
-        //    }
-
-        // GET: CommentsController/Delete/5
-        //public IActionResult Delete(int id)
-        //{
-        //    int userId = GetCurrentUserProfileId();
-        //    Comment comment = _commentRepository.GetCommentById(id);
-
-        //    if (comment.UserProfileId != userId || comment == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    return View(comment);
-        //}
-
-        // POST: CommentsController/Delete/5
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Delete(int id, Comment comment)
-        //{
-        //    try
-        //    {
-        //        int userId = GetCurrentUserProfileId();
-        //        Comment aComment = _commentRepository.GetCommentById(id);
-        //        List<Post> posts = _postRepository.GetUserPostsById(userId);
-        //        foreach (Post aPost in posts)
-        //        {
-        //            if (aPost.Id == aComment.PostId)
-        //            {
-        //                _commentRepository.DeleteComment(id);
-        //            }
-        //        }
-        //        return RedirectToAction("Index", new { id = aComment.PostId });
-        //    }
-        //    catch
-        //    {
-        //        return View(comment);
-        //    }
-        //}
     }
     
     }
