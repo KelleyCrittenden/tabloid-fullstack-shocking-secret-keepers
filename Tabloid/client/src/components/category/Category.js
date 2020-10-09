e7086a0aa450052b2f8b80b2aa37a8fcb61ef285
Brett Stoudt <bstoudt01@gmail.com>
2020-10-09 15:52:42 -0500

    added edit button to category

--- Tabloid/client/src/components/category/Category.js
+++ Tabloid/client/src/components/category/Category.js
@@ -1,12 +1,15 @@
 import React from "react";
-import { Card, CardBody } from "reactstrap";
+import { Card, CardBody, Button } from "reactstrap";
 
 export default function Category({ category }) {
     return (
         <Card>
             <CardBody>
                 <strong>{category.name}</strong>
+                <span >
+                    <Button type="button" id={category.id} href={`/category/${category.id}`}>Edit</Button>
+                </span>
             </CardBody>
-        </Card>
+        </Card >
     );
 }
